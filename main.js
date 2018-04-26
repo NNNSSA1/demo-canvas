6d3c9cdb7da3a5f1f11fb8d0282a14e85ac77edf
wade <352912788@qq.com>
2018-04-26 18:04:07 +0800

    1

--- main.js
+++ main.js
@@ -4,6 +4,14 @@ window.onload = function(){
     var eraser = document.getElementById("eraser")
     var brush = document.getElementById("brush")
     var actions = document.getElementById("actions")
+    var pink = document.getElementById("pink")
+    var blue = document.getElementById("blue")
+    var yellow = document.getElementById("yellow")
+    var lineWidth = 2
+    var thin = document.getElementById("thin")
+    var thick = document.getElementById("thick")
+    var clear = document.getElementById("clear")
+    var save = document.getElementById("save")
     //1.动态设置画布全屏的大小
     autoSize(canvas)
 
@@ -13,14 +21,76 @@ window.onload = function(){
 
     // 3.设置橡皮檫的状态          //尽量只让一个按钮做一个事情。能不加ifelse就不要加，用状态转换更好。
     var eraserEnabled = false
-    eraser.onclick = function (){
-        eraserEnabled = true
-        actions.className = "actions x"
-    }
-    brush.onclick = function (){
+    brush.onclick = function(){
         eraserEnabled = false
-        actions.className = "actions"
-    }    
+        brush.classList.add('active')
+        eraser.classList.remove("active")
+    }
+    eraser.onclick = function(){
+        eraserEnabled = true
+        eraser.classList.add('active')
+        brush.classList.remove("active")
+    }
+    //4.设置画笔的颜色和属性
+    black.onclick = function(){
+        fillStyle = "black"
+        context.strokeStyle = "black"
+        black.classList.add("active")
+        green.classList.remove("active")
+        blue.classList.remove("active")
+        pink.classList.remove("active")
+    }
+
+    pink.onclick = function(){
+        fillStyle = "pink"
+        context.strokeStyle = "pink"
+        pink.classList.add("active")
+        green.classList.remove("active")
+        blue.classList.remove("active")
+        black.classList.remove("active")
+
+    }
+    green.onclick = function(){
+        fillStyle = "green"
+        context.strokeStyle = "green"
+        green.classList.add("active")
+        pink.classList.remove("active")
+        blue.classList.remove("active")
+        black.classList.remove("active")
+
+    }
+    blue.onclick = function(){
+        fillStyle = "blue"
+        context.strokeStyle = "blue"
+        blue.classList.add("active")
+        green.classList.remove("active")
+        pink.classList.remove("active")
+        black.classList.remove("active")
+
+    }
+    //设置画笔的粗细
+    thin.onclick = function(){
+        lineWidth = 2
+        thin.classList.add("active") 
+    }
+    thick.onclick = function(){
+        lineWidth = 5
+ 
+    }
+    //设置清屏
+    clear.onclick = function(){
+        context.clearRect(0,0,canvas.width,canvas.height)
+    }
+    //设置下载保存
+    save.onclick = function(){
+        var url = canvas.toDataURL("image/png")
+        var a = document.createElement("a")
+        document.body.appendChild(a)
+        a.href = url
+        a.download = "作品"
+        a.click()
+    }
+
 
 
 /********工具函数************/
@@ -42,7 +112,7 @@ function autoSize(canvas){          //设置参数，下面的函数中只需要
 function drawLine(x1,y1,x2,y2){  //画线的函数
     context.beginPath()
     context.moveTo(x1,y1)  //起点
-    context.lineWidth = 5;
+    context.lineWidth = lineWidth;
     context.lineTo(x2,y2)  //终点
     context.stroke()
     context.closePath()
@@ -119,5 +189,4 @@ function listenToUser(canvas){
 }
 }
 }
-
 
